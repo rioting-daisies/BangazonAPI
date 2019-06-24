5a4ca2187221e39f5c9a71042fa08c446eb8f28d
Alex Thacker <thacker.alex@yahoo.com>
2019-06-24 13:12:40 -0500

    need a commit

---
+++ BangazonAPI/Models/ProductType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class ProductType
+    {
+        [Required]
+        public int Id { get; set;  }
+        [Required]
+        public string Name { get; set;  }
+    }
+}
+
+//class created by Alex
