e3ee93189cbf152f8675cacb86b6f4ca7c720ced
William Mitchell <wemitchell3@gmail.com>
2019-06-24 16:05:35 -0500

    adding get and get1 method to product type controller

---
+++ BangazonAPI/Controllers/ProductTypeController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BangazonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductTypeController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public ProductTypeController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // GET: api/ProductType
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            string sql = "SELECT Id, Name FROM ProductType";
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    SqlDataReader reader = await cmd.ExecuteReaderAsync();
+                    List<ProductType> productTypes = new List<ProductType>();
+
+                    while (reader.Read())
+                    {
+                        ProductType productType = new ProductType()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name"))                            
+                        };
+                        productTypes.Add(productType);
+                    }
+                    reader.Close();
+                    return Ok(productTypes);
+                }
+            }
+        }
+
+        // GET: api/ProductType/5
+        [HttpGet("{id}")]
+        //[HttpGet("{id}", Name = "Get")]
+        public async Task<IActionResult> Get(int id)
+        {
+            if (!ProductTypeExists(id))
+            {
+                return new StatusCodeResult(StatusCodes.Status404NotFound);
+            }
+            string sql = "SELECT Id, Name FROM ProductType WHERE Id = @id";
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = await cmd.ExecuteReaderAsync();
+                    ProductType productType = null;
+
+                    if (reader.Read())
+                    {
+                        productType = new ProductType()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),                            
+                            Name = reader.GetString(reader.GetOrdinal("Name"))                            
+                        };
+                    }
+                    reader.Close();
+                    return Ok(productType);
+                }
+            }
+        }
+
+        // POST: api/ProductType
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/ProductType/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        private bool ProductTypeExists(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id From ProductType where Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    return reader.Read();
+                }
+            }
+        }
+    }
+}
