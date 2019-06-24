39c4d420f4257dd2747c5cf0dc949a58c1ba25b5
Chris Morgan <bluemorgan2323@gmail.com>
2019-06-24 16:31:00 -0500

    Created tests for both get methods

--- TestBangazonAPI/TestCustomers.cs
+++ TestBangazonAPI/TestCustomers.cs
@@ -1,40 +1,57 @@
-//using System;
-//using System.Net;
-//using Newtonsoft.Json;
-//using Xunit;
-//using BangazonAPI.Models;
-//using System.Threading.Tasks;
-//using System.Collections.Generic;
+using System;
+using System.Net;
+using Newtonsoft.Json;
+using Xunit;
+using BangazonAPI.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
-//namespace TestBangazonAPI
-//{
-//    public class TestCustomers
-//    {
-//        [Fact]
-//        public async Task Test_Get_All_Customers()
-//        {
-//            using (var client = new APIClientProvider().Client)
-//            {
-//                /*
-//                    ARRANGE
-//                */
-
-
-//                /*
-//                    ACT
-//                */
-//                var response = await client.GetAsync("/api/customers");
+namespace TestBangazonAPI
+{
+    public class TestCustomers
+    {
+        [Fact]
+        public async Task Test_Get_All_Customers()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    ARRANGE
+                */
 
 
-//                string responseBody = await response.Content.ReadAsStringAsync();
-//                var customers = JsonConvert.DeserializeObject<List<Customer>>(responseBody);
+                /*
+                    ACT
+                */
+                var response = await client.GetAsync("/api/Customer");
 
-//                /*
-//                    ASSERT
-//                */
-//                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-//                Assert.True(customers.Count > 0);
-//            }
-//        }
-//    }
-//}
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var customers = JsonConvert.DeserializeObject<List<Customer>>(responseBody);
+
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.True(customers.Count > 0);
+            }
+        }
+
+        [Fact]
+        public async Task Test_Get_Single_Customer()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                var response = await client.GetAsync("api/Customer/1");
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var customer = JsonConvert.DeserializeObject<Customer>(responseBody);
+
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.Equal("Sermour", customer.FirstName);
+                Assert.Equal("Butts", customer.LastName);
+                Assert.NotNull(customer);
+            }
+        }
+    }
+}
