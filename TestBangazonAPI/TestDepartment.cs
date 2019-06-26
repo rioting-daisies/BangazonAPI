7038a831b9e3263971a4049d0366642fe874eea8
unknown <cliftonmatuszewski@gmail.com>
2019-06-26 14:30:26 -0500

    Finished department controller starting on all tests for department next

---
+++ TestBangazonAPI/TestDepartment.cs
@@ -0,0 +1,31 @@
+﻿//Author: CLIFTON MATUSZEWSKI
+using System;
+using System.Net;
+using Newtonsoft.Json;
+using Xunit;
+using BangazonAPI.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace TestBangazonAPI
+{
+   public class TestDepartment
+    {
+        //Testing to see if getting all the products from the database works
+        public async Task Test_Get_All_Departments()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                var response = await client.GetAsync("api/Department");
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var products = JsonConvert.DeserializeObject<List<Department>>(responseBody);
+
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.True(products.Count > 0);
+            }
+        }
+    }
+}
