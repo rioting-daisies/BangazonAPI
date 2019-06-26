532f18962709bc2d388b9ceb50eddebef9e9aa87
unknown <cliftonmatuszewski@gmail.com>
2019-06-26 14:59:11 -0500

    Just a couple more tests needed for department control and departmenttests to be completed

--- TestBangazonAPI/TestDepartment.cs
+++ TestBangazonAPI/TestDepartment.cs
@@ -14,6 +14,7 @@ namespace TestBangazonAPI
    public class TestDepartment
     {
         //Testing to see if getting all the products from the database works
+        [Fact]
         public async Task Test_Get_All_Departments()
         {
             using (var client = new APIClientProvider().Client)
@@ -27,5 +28,36 @@ namespace TestBangazonAPI
                 Assert.True(products.Count > 0);
             }
         }
+
+        [Fact]
+        public async Task Test_Get_Single_Department()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                var response = await client.GetAsync("api/Department/1");
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var department = JsonConvert.DeserializeObject<List<Department>>(responseBody);
+
+                foreach (Department d in department)
+                {
+                    Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                    Assert.Equal("HR", d.Name);
+                    Assert.Equal(25000, d.Budget);
+                    Assert.NotNull(d);
+                }
+            }
+        }
+        //Testing to see if correct exception is thrown if nonexistant product id is searched
+        [Fact]
+        public async Task Test_Get_Nonexistant_Department()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                var response = await client.GetAsync("api/Department/999999999");
+                Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+            }
+        }
+
     }
 }
