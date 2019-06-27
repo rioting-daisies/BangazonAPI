5c924de46bfe7ca7e0915857b8f4e53efa4e3b43
unknown <cliftonmatuszewski@gmail.com>
2019-06-27 14:24:51 -0500

    Finished computer controller and all tests associated with it

--- BangazonAPI/Controllers/ComputerController.cs
+++ BangazonAPI/Controllers/ComputerController.cs
@@ -1,11 +1,227 @@
-﻿using System;
+﻿//AUTHOR: CLIFTON MATUSZEWSKI
+
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
-
+using BangazonAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 namespace BangazonAPI.Controllers
 {
-    public class ComputerController
+    //Setting connection route for api/Computer
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComputerController : ControllerBase
     {
+        private readonly IConfiguration _config;
+
+        public ComputerController(IConfiguration config)
+        {
+            _config = config;
+        }
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+        // Get api/Computer Getting all computers from database
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            string sql = "SELECT Id, PurchaseDate, DecomissionDate, Make, Manufacturer FROM Computer";
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    SqlDataReader reader = await cmd.ExecuteReaderAsync();
+                    List<Computer> computers = new List<Computer>();
+
+                    while (reader.Read())
+                    {
+                        Computer computer = new Computer()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            PurchaseDate = reader.GetDateTime(reader.GetOrdinal("PurchaseDate")),
+                            DecomissionDate = reader.GetDateTime(reader.GetOrdinal("DecomissionDate")),
+                            Make = reader.GetString(reader.GetOrdinal("Make")),
+                            Manufacturer = reader.GetString(reader.GetOrdinal("Manufacturer"))
+                        };
+                        computers.Add(computer);
+                    }
+                    reader.Close();
+                    return Ok(computers);
+                }
+            }
+
+        }
+        // GET: api/Computer/5 Getting one computer by Id from database
+
+        [HttpGet("{id}")]
+        //[HttpGet("{id}", Name = "Get")]
+        public async Task<IActionResult> Get(int id)
+        {
+            if (!ComputerExists(id))
+            {
+                return new StatusCodeResult(StatusCodes.Status404NotFound);
+            }
+            string sql = "SELECT Id, PurchaseDate, DecomissionDate, Make, Manufacturer FROM Computer WHERE Id = @id";
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = await cmd.ExecuteReaderAsync();
+                    Computer computer = null;
+
+                    if (reader.Read())
+                    {
+                        computer = new Computer()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            PurchaseDate = reader.GetDateTime(reader.GetOrdinal("PurchaseDate")),
+                            DecomissionDate = reader.GetDateTime(reader.GetOrdinal("DecomissionDate")),
+                            Make = reader.GetString(reader.GetOrdinal("Make")),
+                            Manufacturer = reader.GetString(reader.GetOrdinal("Manufacturer"))
+                        };
+                    }
+                    reader.Close();
+                    return Ok(computer);
+                }
+            }
+        }
+
+        //Post api/computer Posting new computer to the database
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Computer computer)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO Computer (PurchaseDate, 
+                                                DecomissionDate,
+                                                Make, 
+                                                Manufacturer)
+                                                 OUTPUT INSERTED.Id
+                                                VALUES(@PurchaseDate, @DecomissionDate, @Make, @Manufacturer)";
+                    cmd.Parameters.Add(new SqlParameter("@PurchaseDate", computer.PurchaseDate));
+                    cmd.Parameters.Add(new SqlParameter("@DecomissionDate", computer.DecomissionDate));
+                    cmd.Parameters.Add(new SqlParameter("@Make", computer.Make));
+                    cmd.Parameters.Add(new SqlParameter("@Manufacturer", computer.Manufacturer));
+                    
+
+                    computer.Id = (int)await cmd.ExecuteScalarAsync();
+
+                    return CreatedAtAction("Get", new { id = computer.Id }, computer);
+                }
+            }
+        }
+
+        //PUT api/computer/5 Editing computer from database using Computer Id to target
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([FromRoute] int id, [FromBody] Computer computer)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"UPDATE Computer
+                                          SET PurchaseDate = @PurchaseDate,
+                                              DecomissionDate = @DecomissionDate,
+                                              Make = @Make,
+                                              Manufacturer = @Manufacturer
+                                                WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+                        cmd.Parameters.Add(new SqlParameter("@PurchaseDate", computer.PurchaseDate));
+                        cmd.Parameters.Add(new SqlParameter("@DecomissionDate", computer.DecomissionDate));
+                        cmd.Parameters.Add(new SqlParameter("@Make", computer.Make));
+                        cmd.Parameters.Add(new SqlParameter("@Manufacturer", computer.Manufacturer));
+
+                        int rowsAffected = await cmd.ExecuteNonQueryAsync();
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!ComputerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+        //DELETE api/Computer/1 Delete Computer from databased using Id as target value
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"DELETE FROM Computer WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        int rowsAffected = await cmd.ExecuteNonQueryAsync();
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!ComputerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+        //Method to see if Product with certain Id exists
+        private bool ComputerExists(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id From Computer where Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    return reader.Read();
+                }
+            }
+        }
     }
 }
