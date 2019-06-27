1b40298b0e779336ad684c3d73b4a6dc4c272edd
unknown <cliftonmatuszewski@gmail.com>
2019-06-27 15:18:20 -0500

    Oops

---
+++ BangazonAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,255 @@
+﻿//AUTHOR: CLIFTON MATUSZEWSKI
+
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+namespace BangazonAPI.Controllers
+{
+    //Setting connection route for api/Employee
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public EmployeeController(IConfiguration config)
+        {
+            _config = config;
+        }
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+        // Get api/Product Getting all Employees from database
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            string sql = @"SELECT e.Id AS Id, 
+                          e.FirstName AS FirstName, 
+                            e.LastName AS LastName, 
+                            e.DepartmentId AS DepartmentId,
+                            e.IsSuperVisor AS IsSuperVisor 
+                          FROM Employee e
+                          JOIN Department d ON e.DepartmentId = d.Id";
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    SqlDataReader reader = await cmd.ExecuteReaderAsync();
+                    List<Employee> employees = new List<Employee>();
+                        Department department = null;
+                    
+
+                    while (reader.Read())
+                    {
+
+                        Employee employee = new Employee()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            department = new Department() {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                Budget = reader.GetInt32(reader.GetOrdinal("Budget"))
+                            }
+                        };
+                        employees.Add(employee);
+                    }
+                    reader.Close();
+                    return Ok(employees);
+                }
+            }
+
+        }
+        // GET: api/Employee/5 Getting one employee by Id from database
+
+        [HttpGet("{id}")]
+        //[HttpGet("{id}", Name = "Get")]
+        public async Task<IActionResult> Get(int id)
+        {
+            if (!EmployeeExists(id))
+            {
+                return new StatusCodeResult(StatusCodes.Status404NotFound);
+            }
+            string sql = @"SELECT e.Id AS Id, 
+                          e.FirstName AS FirstName, 
+                            e.LastName AS LastName, 
+                            e.DepartmentId AS DepartmentId,
+                            e.IsSuperVisor AS IsSuperVisor
+                          FROM Employee e
+                          JOIN Department d ON e.DepartmentId = d.Id WHERE e.Id = @id";
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = await cmd.ExecuteReaderAsync();
+                    Employee employee = null;
+
+                    if (reader.Read())
+                    {
+                        employee = new Employee()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            department = new Department()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                Budget = reader.GetInt32(reader.GetOrdinal("Budget"))
+                            }
+                        };
+                    }
+                    reader.Close();
+                    return Ok(employee);
+                }
+            }
+        }
+
+        //Post api/employee Posting new employee to the database
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Employee employee)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO Employee (FirstName, 
+                                             LastName, 
+                                             DepartmentId, 
+                                             IsSuperVisor
+                                             )
+                        OUTPUT INSERTED.Id
+                        VALUES(@FirstName, @LastName, @DepartmentId, @IsSuperVisor)";
+                    cmd.Parameters.Add(new SqlParameter("@FirstName", employee.FirstName));
+                    cmd.Parameters.Add(new SqlParameter("@LastName", employee.LastName));
+                    cmd.Parameters.Add(new SqlParameter("@DepartmentId", employee.DepartmentId));
+                    cmd.Parameters.Add(new SqlParameter("@IsSuperVisor", employee.IsSuperVisor));
+                    
+
+                    employee.Id = (int)await cmd.ExecuteScalarAsync();
+
+                    return CreatedAtAction("Get", new { id = employee.Id }, employee);
+                }
+            }
+        }
+
+        //PUT api/product/5 Editing product from database using Product Id to target
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([FromRoute] int id, [FromBody] Product product)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"UPDATE Product
+                                          SET ProductTypeId = @ProductTypeId,
+                                              CustomerId = @CustomerId,
+                                              Price = @Price,
+                                              Title = @Title,
+                                              Description = @Description,
+                                              Quantity = @Quantity
+                                          WHERE Id = @id";
+
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+                        cmd.Parameters.Add(new SqlParameter("@ProductTypeId", product.ProductTypeId));
+                        cmd.Parameters.Add(new SqlParameter("@CustomerId", product.CustomerId));
+                        cmd.Parameters.Add(new SqlParameter("@Price", product.Price));
+                        cmd.Parameters.Add(new SqlParameter("@Title", product.Title));
+                        cmd.Parameters.Add(new SqlParameter("@Description", product.Description));
+                        cmd.Parameters.Add(new SqlParameter("@Quantity", product.Quantity));
+
+                        int rowsAffected = await cmd.ExecuteNonQueryAsync();
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+        //DELETE api/product/1 Delete product from databased using Id as target value
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"DELETE FROM Product WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        int rowsAffected = await cmd.ExecuteNonQueryAsync();
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+        //Method to see if Product with certain Id exists
+        private bool EmployeeExists(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id From Employee where Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    return reader.Read();
+                }
+            }
+        }
+    }
+}
